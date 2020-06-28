6bebba087fa89ba09496af48b398467b6a867447
George Gichuki <george.gichuki@fdmgroup.com>
2020-06-28 12:48:09 -0500

    Added all the code for converting a String into an ArrayList

--- src/Convert.java
+++ src/Convert.java
@@ -1,4 +1,25 @@
+	import java.util.List;
+	import java.util.ArrayList;
+	import java.util.Arrays;
+	
+public class Convert {	
 
-public class Convert {
+		public static void main(String[] args) {
 
+			String str = "A cheetah is a fast animal, an elephant is a big animal";
+			int len = 3;
+			String str1[] = str.split(",");
+			List<String> list = new ArrayList<String>();
+			list=Arrays.asList(str1);
+		
+			ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(str1));	
+			arrayList.add(0, "bigger");
+	  
+			for (int i=0; i < arrayList.size(); i++) {
+				System.out.println(arrayList.get(i));
+			}
+	    
+
+		}
+	
 }
